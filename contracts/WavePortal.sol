ff0444863c2ccd7a58b22cd729d8431ef53d72f3
stackrev <farbitherast5888@gmail.com>
2023-07-05 06:19:57 +0200

    call public functions of contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,10 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
-    function greeting() public view returns (string memory){
-      return "hello";
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
